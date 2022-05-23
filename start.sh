4fa4270b147ed5e1fdc9114fde6987c39b0ea68e
lvlinkeji <ab9654321@163.com>
2022-05-23 11:19:39 +0800

    ok

--- start.sh
+++ start.sh
@@ -5,15 +5,16 @@ chmod -Rf 777 /run/screen
 #/usr/local/bin/ttyd -p $PORT -c admin:adminks123 bash
 
 #nginx
+sed -i "s|iPORT|$PORT|g" /etc/nginx/sites-available/default
 sed -i 's/#gzip[ ]on;/gzip on;/g' /etc/nginx/nginx.conf
 sed -i 's/client_max_body_size[ ]1m;/client_max_body_size 0;/g' /etc/nginx/nginx.conf
 
 #run ttyd
-screen_name="ttyd"
-screen -dmS $screen_name
-cmd="/usr/local/bin/ttyd -p 7681 -c root:c68.300OQa bash";
-screen -x -S $screen_name -p 0 -X stuff "$cmd"
-screen -x -S $screen_name -p 0 -X stuff '\n'
+# screen_name="ttyd"
+# screen -dmS $screen_name
+# cmd="/usr/local/bin/ttyd -p 7681 -c root:c68.300OQa bash";
+# screen -x -S $screen_name -p 0 -X stuff "$cmd"
+# screen -x -S $screen_name -p 0 -X stuff '\n'
 
 filebrowser config init
 filebrowser config set -b '/file'
@@ -21,19 +22,19 @@ filebrowser config set -p 60002
 filebrowser users add root c68.300OQa --perm.admin
 
 #run filebrowser
-screen_name="filebrowser"
-screen -dmS $screen_name
-cmd="filebrowser -r /";
-screen -x -S $screen_name -p 0 -X stuff "$cmd"
-screen -x -S $screen_name -p 0 -X stuff '\n'
+# screen_name="filebrowser"
+# screen -dmS $screen_name
+# cmd="filebrowser -r /";
+# screen -x -S $screen_name -p 0 -X stuff "$cmd"
+# screen -x -S $screen_name -p 0 -X stuff '\n'
 #filebrowser username:admin password:admin
 
 #run rclone
-screen_name="rclone"
-screen -dmS $screen_name
-cmd="rclone rcd --rc-web-gui --rc-addr 127.0.0.1:5572 --rc-user root --rc-pass c68.300OQa";
-screen -x -S $screen_name -p 0 -X stuff "$cmd"
-screen -x -S $screen_name -p 0 -X stuff '\n'
+# screen_name="rclone"
+# screen -dmS $screen_name
+# cmd="rclone rcd --rc-web-gui --rc-addr 127.0.0.1:5572 --rc-user root --rc-pass c68.300OQa";
+# screen -x -S $screen_name -p 0 -X stuff "$cmd"
+# screen -x -S $screen_name -p 0 -X stuff '\n'
 
 supervisord -c /supervisord.conf
 
